88a3cb4b9765f673eedf6dc1cac0ea25518b1631
jakob12317 <jakob.bonde.nielsen@gmail.com>
2018-04-14 12:10:20 +0200

    Repository set up
    see above

--- src/CQRS/CQRS/Repository.cs
+++ src/CQRS/CQRS/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,26 @@ namespace CQRS
         public Repository(CQRSContext context)
         {
             _context = context;
+        }
+
+        public TEntity Get(Guid id)
+        {
+            return _context; 
+        }
+
+        public void Add(TEntity entity)
+        {
+            
+        }
+
+        public void Delete(TEntity entity)
+        {
+
+        }
+
+        public void Edit(TEntity entity)
+        {
+
         }
 
     }
