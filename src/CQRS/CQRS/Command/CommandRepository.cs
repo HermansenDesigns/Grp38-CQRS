83d2eea0db50022c21ce67e9fae8a282f6ec048e
Martin-Dalgaard <mldalgaard@gmail.com>
2018-04-16 01:54:28 +0200

    statevejl

--- src/CQRS/CQRS/Command/CommandRepository.cs
+++ src/CQRS/CQRS/Command/CommandRepository.cs
@@ -25,12 +25,14 @@ namespace CQRS.Command
 
         public void CheckVersionAndAddItem(Func<TEntity, bool> where, TEntity entity)
         {
-            var colllection = Find(where);
+            //Create collection with commands matching our entity.
+            var collection = Find(where);
             var expectedVersion = 0;
-            if (colllection.Any())
+            if (collection.Any())
             {
-                expectedVersion = colllection.ToList().OrderBy(item => item.Version).Last().Version;
+                expectedVersion = collection.ToList().OrderBy(item => item.Version).Last().Version;
             }
+            //Test if version supplied by command matches the latest version +1
             if (entity.Version != (expectedVersion + 1))
                 throw new ArgumentException();
             AddItem(entity);
