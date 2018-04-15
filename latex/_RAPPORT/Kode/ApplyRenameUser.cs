f4164fc33f61f48f4495bd458ecc87fa1efef434
Martin-Dalgaard <mldalgaard@gmail.com>
2018-04-16 01:50:33 +0200

    More commits

---
+++ latex/_RAPPORT/Kode/ApplyRenameUser.cs
@@ -0,0 +1,7 @@
+﻿public void Apply(RenameUserCommand entity){
+    var user = _context.UserDisplays.Find(entity.UserId);
+    user.Name = entity.NewName;
+    user.LatestRenameUserCommand = entity;
+    _context.SaveChanges();
+}
+    
