83c7c5890cf0935d5e9e0e377bdadff8683a58ff
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-04-16 04:16:56 +0200

    Blabla

--- latex/_RAPPORT/Kode/ApplyRenameUser.cs
+++
@@ -1,9 +0,0 @@
-﻿public void Apply(RenameUserCommand entity){
-    //Find user relevant to command in local collection
-    var user = _context.UserDisplays.Find(entity.UserId);
-    //Update local version of user
-    user.Name = entity.NewName;
-    user.LatestRenameUserCommand = entity;
-    _context.SaveChanges();
-}
-    
